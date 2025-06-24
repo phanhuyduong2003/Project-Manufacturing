2e06e1f5bb3208498fdcc2de929fef239cc78266
phanhuyduong2003 <huyduongphan8@gmail.com>
2025-06-24 23:17:07 +0700

    [Update] - Config + Layout

--- src/types/common/index.ts
+++ src/types/common/index.ts
@@ -2,5 +2,15 @@ import { SVGProps } from "react";
 
 export interface IconProps extends SVGProps<SVGSVGElement> {
   size?: number;
+  viewBox?: string;
   className?: string;
+}
+
+export interface HeaderProps {
+  title: string;
+  className?: {
+    wrapper?: string;
+    icon?: string;
+    title?: string;
+  };
 }
